302adddd17b59d1cd08fb7469fc9ebf2d2feac6d
Mateus Ito <contact.matthew.ito@gmail.com>
2025-05-26 12:58:06 -0300

    refactor(entities): move repositories to entities package

--- internal/entities/message.go
+++ internal/entities/message.go
@@ -9,3 +9,15 @@ type Message struct {
 	ReactionCount int64     `json:"reaction_count"`
 	Answered      bool      `json:"answered"`
 }
+
+type MessageRepository interface {
+	GetAllMessages() []Message
+	GetMessageByID(id uuid.UUID) (Message, error)
+	GetMessagesByRoomID(roomID uuid.UUID) ([]Message, error)
+	CreateMessage(roomID uuid.UUID, message string) (Message, error)
+	DeleteMessage(id uuid.UUID) error
+	UpdateMessage(id uuid.UUID, message string) (Message, error)
+	ReactToMessage(id uuid.UUID) (Message, error)
+	RemoveReactionFromMessage(id uuid.UUID) (Message, error)
+	MarkMessageAsAnswered(id uuid.UUID) (Message, error)
+}
--- internal/entities/room.go
+++ internal/entities/room.go
@@ -6,3 +6,11 @@ type Room struct {
 	ID    uuid.UUID `json:"id"`
 	Theme string    `json:"theme"`
 }
+
+type RoomRepository interface {
+	GetAllRooms() ([]Room, error)
+	GetRoomByID(id uuid.UUID) (Room, error)
+	CreateRoom(theme string) (Room, error)
+	DeleteRoom(id uuid.UUID) error
+	UpdateRoom(id uuid.UUID, theme string) (Room, error)
+}
